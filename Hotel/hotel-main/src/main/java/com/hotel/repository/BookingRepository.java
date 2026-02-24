23eed310a27f70edcd1dcf3a6b5c36fd450c3518
Юрий <sums555@mail.ru>
2026-02-24 03:01:37 +0300

    Добавление Liquibase и исправление N+1
    Исправил N+1 в методах FindAll, где помимо основного класса подгружались данные из сторонних таблиц по одной строке

--- Hotel/hotel-main/src/main/java/com/hotel/repository/BookingRepository.java
+++ Hotel/hotel-main/src/main/java/com/hotel/repository/BookingRepository.java
@@ -63,9 +63,16 @@ public class BookingRepository extends BaseRepository<Booking,Long> {
     protected Long getId(Booking booking) {
         return booking.getId();
     }
-
+    @Override
+    public List<Booking> findAll() {
+        Session session = HibernateUtil.getSession();
+        return session.createQuery("select b from Booking b " +
+                        "join fetch b.room " +
+                        "join fetch b.client",
+                Booking.class)
+                .getResultList();
+    }
     public List<Booking> threeBookingByRoom(int roomNumber) {
-        List<Booking> bookings = new ArrayList<>();
         Session session = HibernateUtil.getSession();
             return session.createQuery(
                             """
